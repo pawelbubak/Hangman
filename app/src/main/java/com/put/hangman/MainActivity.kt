96369977162a06338b605d2226edae9ed85de764
Paweł Bubak <pawelbubak97@gmail.com>
2019-05-29 19:16:25 +0200

    Add login activity

---
+++ app/src/main/java/com/put/hangman/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.put.hangman
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    private val REQUEST_CODE: Int = 1
+    private var providers: List<AuthUI.IdpConfig>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initProviders()
+
+        showSignInOptions()
+    }
+
+    override
+    fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE) {
+            val response: IdpResponse? = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                val user: FirebaseUser? = FirebaseAuth.getInstance().currentUser
+                Toast.makeText(this, user?.email, Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, response?.error?.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun initProviders() {
+        providers = Arrays.asList(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+    }
+
+    private fun showSignInOptions() {
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers!!)
+                .setTheme(R.style.AppTheme)
+                .build(),
+            REQUEST_CODE
+        )
+    }
+
+    private fun logout() {
+        AuthUI.getInstance().signOut(this@MainActivity).addOnCompleteListener {
+            showSignInOptions()
+        }.addOnFailureListener {
+            Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
